51de7f703f0e6f52b55004a45e13cbefc3c8cac3
Carlos Ferrer <mundofuturoca@gmail.com>
2025-12-03 19:20:06 -0300

    added react hook state

--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-const tech = 'React and Vite'
-const theme = {
-    primary: 'red',
-    secondary: 'blue'
-}
-export function App (){
-    return <h1 style={{background:theme.primary, color: 'white'}} className='font-mono text-4xl'>Hello from {tech}!!</h1>
-}
---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import { PageWrapper } from "./components/PageWrapper";
+import { Container } from "./components/Container";
+import { Header } from "./components/Header";
+import { Search } from "./components/Search";
+import { Shortlist } from "./components/Shorlist";
+import { PuppiesList } from "./components/PuppiesList";
+import { NewPuppyForm } from "./components/NewPuppyForm";
+
+import { puppies } from "./data/puppies"
+
+
+export function App() {
+    return (
+        <PageWrapper>
+            <Container>
+                <Header />
+                <main>
+                    <Main />
+                </main>
+            </Container>
+        </PageWrapper>
+
+    )
+}
+
+function Main() {
+    return (
+        <>
+            {/* Search & Shortlist */}
+            <div className="mt-24 grid gap-8 sm:grid-cols-2">
+                {/* Search */}
+                <Search />
+                {/* Shortlist */}
+                <Shortlist/>
+            </div>
+            {/* Puppies list */}
+            <PuppiesList puppies={puppies} />
+            {/* New Puppy form */}
+            <NewPuppyForm />
+        </>
+
+    )
+}
