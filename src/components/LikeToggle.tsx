055bf1b18bb7ff5e48cd43ac423f0a106c78a702
Prof. Acenaga <carlos.ferrer@davinci.edu.ar>
2026-01-05 12:41:24 -0300

    typescript & first taste insteractivity

--- src/components/LikeToggle.tsx
+++ src/components/LikeToggle.tsx
@@ -7,7 +7,18 @@ export function LikeToggle() {
 
     function handleClick() {
         setIsLiked(!isLiked);
+        //increment count by 1 when i make click
         setCount(count + 1);
+        // in this part we want to increment the count by 3 when liked, but we mistakenly call setCount multiple times
+        // setCount(count + 1);
+        // setCount(count + 1);
+        // setCount(count + 1);
+        //-------------------------------
+        // Correct way to increment by 3
+        // using a callback function to ensure we get the latest state
+        // setCount((prevCount) => prevCount + 1);
+        // setCount((prevCount) => prevCount + 1);
+        // setCount((prevCount) => prevCount + 1);
     }
     return (
         <button className="group flex items-center gap-1" onClick={handleClick}>
